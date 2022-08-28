4b15ed27997853ff33aae1c1d458abcb474b9372
Gm-shuvo <gm.shuvo.ru.cse@gmail.com>
2022-08-28 17:06:22 +0600

    Setup laravel Project with vite and tailwindcss

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel-Demo</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        @vite ('resources/js/app.js')
+    </head>
+    <body>
+        <div class="p-6 bg-green border-b border-gray-700 text-md">
+            <h1>Gm-shuvo</h1>
+        </div>
+    </body>
+    
+</html>
